ed4e00c0f4af75ac99b4e5832ff752074aaa23aa
Mattia Roccoberton <mattia.roccoberton@telepass.com>
2023-04-10 12:21:53 +0200

    feat: improve default layout

--- lib/tiny_admin/views/actions/index.rb
+++ lib/tiny_admin/views/actions/index.rb
@@ -5,7 +5,7 @@ module TinyAdmin
     module Actions
       class Index < DefaultLayout
         attr_reader :current_page, :fields, :pages, :prepare_record, :records
-        attr_accessor :actions, :filters
+        attr_accessor :actions, :filters, :query_string
 
         def setup_pagination(current_page:, pages:)
           @current_page = current_page
